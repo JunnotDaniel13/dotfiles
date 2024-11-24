7a92791c6a3835cf7f5e41ffe4fd34ae0e4f4e6c
Junnot Daniel <jerenasonjunnot@gmail.com>
2024-11-24 18:21:23 +0300

    update hyprland

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-ln -s ~/dotfiles/scripts ~/.config
+rm -rf ~/.config/kitty && ln -s ~/dotfiles/scripts ~/.config
 ln -s ~/dotfiles/starship.toml ~/.config
-ln -s ~/dotfiles/kitty ~/.config
-ln -s ~/dotfiles/hypr ~/.config
-ln -s ~/dotfiles/wofi ~/.config
+rm -rf ~/.config/kitty && ln -s ~/dotfiles/kitty ~/.config
+rm -rf ~/.config/hypr && ln -s ~/dotfiles/hypr ~/.config
+rm -rf ~/.config/wofi && ln -s ~/dotfiles/wofi ~/.config
 ln -s ~/dotfiles/themes ~/.config
 ln -s ~/dotfiles/backgrounds ~/config
 ln -s ~/dotfiles/waybar ~/.config
