8a3d51cc4b4a782cd6be6ef4a7797701def63ec5
Junnot Daniel <jerenasonjunnot@gmail.com>
2024-11-24 19:56:01 +0300

    fix config folder name

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ rm -rf ~/.config/kitty && ln -s ~/dotfiles/kitty ~/.config
 rm -rf ~/.config/hypr && ln -s ~/dotfiles/hypr ~/.config
 rm -rf ~/.config/wofi && ln -s ~/dotfiles/wofi ~/.config
 ln -s ~/dotfiles/themes ~/.config
-ln -s ~/dotfiles/backgrounds ~/config
+ln -s ~/dotfiles/backgrounds ~/.config
 ln -s ~/dotfiles/waybar ~/.config
 ln -s ~/dotfiles/swaylock ~/.config
 ln -s ~/dotfiles/wlogout ~/.config
