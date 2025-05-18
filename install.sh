1577a12a2d2fd4d826e60a8c71c8ac68c96172ff
Junnot Daniel <jerenasonjunnot@gmail.com>
2025-05-18 17:10:49 +0300

    fix install script to correctly remove and link configuration files

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
-rm -rf ~/.config/kitty && ln -s ~/dotfiles/scripts ~/.config
-ln -s ~/dotfiles/starship.toml ~/.config
+rm -rf ~/.config/scripts && ln -s ~/dotfiles/scripts ~/.config
+rm -rf ~/.config/starship.toml && ln -s ~/dotfiles/starship.toml ~/.config
 rm -rf ~/.config/kitty && ln -s ~/dotfiles/kitty ~/.config
 rm -rf ~/.config/hypr && ln -s ~/dotfiles/hypr ~/.config
 rm -rf ~/.config/wofi && ln -s ~/dotfiles/wofi ~/.config
-ln -s ~/dotfiles/themes ~/.config
-ln -s ~/dotfiles/backgrounds ~/.config
-ln -s ~/dotfiles/waybar ~/.config
-ln -s ~/dotfiles/swaylock ~/.config
-ln -s ~/dotfiles/wlogout ~/.config
+rm -rf ~/.config/themes && ln -s ~/dotfiles/themes ~/.config
+rm -rf ~/.config/backgrounds && ln -s ~/dotfiles/backgrounds ~/.config
+rm -rf ~/.config/waybar && ln -s ~/dotfiles/waybar ~/.config
